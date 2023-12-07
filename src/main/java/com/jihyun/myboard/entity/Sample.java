6b7188dac055277bad33dcbfb3680b862132839f
jihyun <372491@naver.com>
2023-12-07 17:59:40 +0900

    메인 엔티티 클래스 생성

---
+++ src/main/java/com/jihyun/myboard/entity/Sample.java
@@ -0,0 +1,15 @@
+package com.jihyun.myboard.entity;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.stereotype.Service;
+
+@Getter
+@Setter
+public class Sample {
+
+    private String name;
+    private String phone;
+    private String email;
+    private String address;
+}
