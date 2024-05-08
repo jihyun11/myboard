5e3cd94e0edc1c28f611fd7e9b13aaf8a6f9059d
jihyun <372491@naver.com>
2024-05-08 23:23:26 +0900

    #20240508 + 글쓰기 페이지 들어가자마자 권한검증 하기 (admin이 아닌 계정은 로그인 페이지로 내보내기)

--- src/main/java/com/jihyun/myboard/restcontroller/ContentApiController.java
+++ src/main/java/com/jihyun/myboard/restcontroller/ContentApiController.java
@@ -3,6 +3,8 @@ package com.jihyun.myboard.restcontroller;
 import com.jihyun.myboard.dto.ContentDTO;
 import com.jihyun.myboard.exception.MyException;
 import com.jihyun.myboard.service.ContentService;
+import com.jihyun.myboard.token.TokenUtil;
+import io.jsonwebtoken.Claims;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
@@ -19,13 +21,28 @@ public class ContentApiController {
 
 //    @RequestBody 방식으로 보내기
     @PostMapping("/api/content")
-    public ContentDTO getContentDTO(@RequestBody ContentDTO contentDTO) {
-        if (!contentDTO.getRole().equals("admin")) {
+    public ContentDTO getContentDTO(@RequestBody ContentDTO contentDTO,
+                                    @RequestHeader("Authorization") String jwt) {
+        Claims claims = TokenUtil.parseJwtToken(jwt);
+        String tokenRole = (String) claims.get("role");
+        System.out.println(tokenRole);
+
+        if (!tokenRole.equals("admin")) {
             throw new MyException("admin이 아닙니다");
         }
         log.info(contentDTO.getContent(), contentDTO.getAuthor());
         contentService.insertContent(contentDTO.getContent(), contentDTO.getAuthor());
 
         return contentDTO;
+    }
+
+    @PostMapping("/role/check")
+    public String getRole(@RequestBody ContentDTO contentDTO) {
+        TokenUtil tokenUtil = new TokenUtil();
+        String jwt = tokenUtil.makeJwtToken(contentDTO.getAuthor(), contentDTO.getRole());
+        String jwtResult = String.valueOf(tokenUtil.parseJwtToken(jwt));
+        System.out.println(jwt);
+
+        return jwtResult;
     }
 }
