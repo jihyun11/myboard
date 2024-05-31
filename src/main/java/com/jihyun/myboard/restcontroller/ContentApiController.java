685c829f5a4662232b4c0165164f6e3fa2b270f6
jihyun <372491@naver.com>
2024-05-31 16:12:21 +0900

    ### 240531 + 게시글 등록하는 곳에 트랜잭션 적용해 보기

--- src/main/java/com/jihyun/myboard/restcontroller/ContentApiController.java
+++ src/main/java/com/jihyun/myboard/restcontroller/ContentApiController.java
@@ -7,6 +7,7 @@ import com.jihyun.myboard.token.TokenUtil;
 import io.jsonwebtoken.Claims;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.*;
 
 @Slf4j
@@ -20,36 +21,20 @@ public class ContentApiController {
     }
 
 //    @RequestBody 방식으로 보내기
+    @Transactional
     @PostMapping("/api/content")
     // 핵심 관점 - 게시글 저장하는 것
     // 부가적인 관점 - 저장할 때에 사용자의 권한을 확인하는 것
-    public ContentDTO getContentDTO(@RequestBody ContentDTO contentDTO,
-                                    @RequestHeader("Authorization") String jwt) {
-//        Claims claims = TokenUtil.parseJwtToken(jwt);
-//        String tokenRole = (String) claims.get("role");
-//        System.out.println(tokenRole);
-//
-//        if (!tokenRole.equals("admin")) {
-//            throw new MyException("admin이 아닙니다");
-//        }
+    public ContentDTO getContentDTO(@RequestBody ContentDTO contentDTO) {
         log.info(contentDTO.getContent(), contentDTO.getAuthor());
         contentService.insertContent(contentDTO.getContent(), contentDTO.getAuthor());
-
         return contentDTO;
     }
 
-        @PostMapping("/role/check")
-        public String getRole(@RequestBody ContentDTO contentDTO,
-                              @RequestHeader("Authorization") String jwt) {
-//            Claims claims = TokenUtil.parseJwtToken(jwt);
-//            String tokenRole = (String) claims.get("role");
-//            System.out.println(tokenRole);
-//
-//            if (!tokenRole.equals("admin")) {
-//                throw new MyException("admin이 아닙니다");
-//            }
-            log.info(contentDTO.getContent(), contentDTO.getAuthor());
-
-            return "admin이 맞습니다";
+    @Transactional
+    @PostMapping("/role/check")
+    public String getRole(@RequestBody ContentDTO contentDTO) {
+        log.info(contentDTO.getContent(), contentDTO.getAuthor());
+        return "admin이 맞습니다";
     }
 }
